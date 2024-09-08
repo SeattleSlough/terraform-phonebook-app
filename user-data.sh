d6978fafe47a32343994612ff52c8a92314f584d
maggs <runawayserver@infinitelyloopee.dev>
2024-09-08 15:36:32 -0700

    added cd commoand to user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -6,8 +6,9 @@ pip3 install flask-mysql
 dnf install git -y
 TOKEN=${user-data-git-token}
 USER=${user-data-git-user}
+cd /home/ec2-user
 git clone https://github.com/$USER/phonebook-app.git phonebook
 python3 /home/ec2_user/phonebook/phonebook-app.py
 
-# Replace line 9 with https://$TOKEN@github.com/$USER/<repo> to allow for a private repo
+# Replace line 10 with https://$TOKEN@github.com/$USER/<repo> to allow for a private repo
 # I've made the phonebook app repo public so this isn't implemented

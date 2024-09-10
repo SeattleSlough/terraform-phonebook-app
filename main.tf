e959604b3d5ed6cbc73a1101af0b0ca476a42802
maggs <runawayserver@infinitelyloopee.dev>
2024-09-10 14:16:54 -0700

    added asg, rds, route53 to main.tf

--- main.tf
+++ main.tf
@@ -68,3 +68,82 @@ resource "aws_launch_template" "app-asg-lt" {
       }
     }
 }
+
+resource "aws_alb" "app-lb" {
+    name = "phoebook-alb"
+    ip_address_type = "ipv4"
+    internal = false
+    load_balancer_type = "application"
+    security_groups = [ aws_security_group.alb-sg.id ]
+    subnets = data.aws_subnets.default.ids
+}
+
+resource "aws_alb_listener" "app-lb-listener" {
+    load_balancer_arn = aws_alb.app-lb.arn
+    port = 80
+    protocol = "HTTP"
+    default_action {
+      type = "forward"
+      target_group_arn = aws_alb_target_group.app-lb-tg.arn
+    }
+}
+
+resource "aws_autoscaling_group" "app-asg" {
+    min_size = 1
+    max_size = 2
+    desired_capacity = 2
+    name = "phonebook-asg"
+    health_check_grace_period = 300
+    health_check_type = "ELB"
+    target_group_arns = [aws_alb_target_group.app-lb-tg.arn]
+    vpc_zone_identifier = [ aws_alb.app-lb.subnets ]
+    launch_template {
+      id = aws_alb_target_group.app-lb-tg.id
+      version = aws_alb_target_group.app-lb-tg.latest_version
+    }
+}
+
+
+resource "aws_db_instance" "app-rds-mysql" {
+    db_name = "phonebook"
+    instance_class = "db.t3.micro"
+    allocated_storage = 20
+    vpc_security_group_ids = [ aws_security_group.db-sg.id ]
+    engine = "mysql"
+    engine_version = "8.0.35"
+    multi_az = false
+    port = 3306
+    allow_major_version_upgrade = false
+    auto_minor_version_upgrade = true
+    monitoring_interval = 0
+    backup_retention_period = 0
+    publicly_accessible = false
+    skip_final_snapshot = true
+    username = "admin"
+    password = "Oliver_1"
+    identifier = "phonebook-rds-mysql"
+}
+
+resource "github_repository_file" "dbendpoint" {
+    content = aws_db_instance.app-rds-mysql.address
+    file = "dbserver.endpoint"
+    repository = "phonebook-app"
+    overwrite_on_create = true
+    branch = "main" #ensure this is the branch you are using
+}
+
+data "aws_route53_zone" "public" {
+    name = var.hosted-zone
+}
+
+resource "aws_route53_record" "phonebook-url" {
+    zone_id = data.aws_route53_zone.public.id
+    name = "phonebook.${var.hosted-zone}"
+    type = "A"
+
+    alias {
+      name = aws_alb.app-lb.dns_name
+      zone_id = aws_alb.app-lb.zone_id
+      evaluate_target_health = true
+    }
+}

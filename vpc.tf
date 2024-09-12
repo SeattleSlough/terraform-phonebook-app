5989e418f6b697871fa3bc201c0d968bd1e07035
Michael Maggs <runawayserver@seattleslew.com>
2024-09-12 10:55:03 -0700

    commented out vpc.tf module (in progress)

--- vpc.tf
+++ vpc.tf
@@ -1,42 +1,42 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  instance_tenancy = "default"
+# resource "aws_vpc" "main" {
+#   cidr_block = "10.0.0.0/16"
+#   instance_tenancy = "default"
 
-  tags = {
-    Name = "main"
-  }
-}
+#   tags = {
+#     Name = "main"
+#   }
+# }
 
-resource "aws_subnet" "public-1" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.10.0/24"
+# resource "aws_subnet" "public-1" {
+#     vpc_id = aws_vpc.main.id
+#     cidr_block = "10.0.10.0/24"
 
-    tags = {
-        Name = "public-subnet-1"
-    }
-}
+#     tags = {
+#         Name = "public-subnet-1"
+#     }
+# }
 
-resource "aws_subnet" "private-1" {
-    vpc_id = aws_vpc.main.id
+# resource "aws_subnet" "private-1" {
+#     vpc_id = aws_vpc.main.id
 
-    cidr_block = "10.0.20.0/24"
-}
+#     cidr_block = "10.0.20.0/24"
+# }
 
-resource "aws_internet_gateway" "igw" {
-    vpc_id = aws_vpc.main.id
-}
+# resource "aws_internet_gateway" "igw" {
+#     vpc_id = aws_vpc.main.id
+# }
 
- resource "aws_nat_gateway" "ngw" {
-    subnet_id = aws_subnet.public-1.id
+#  resource "aws_nat_gateway" "ngw" {
+#     subnet_id = aws_subnet.public-1.id
 
- }
+#  }
 
- resource "" "name" {
+#  resource "" "name" {
 
- }
+#  }
 
- resource "aws_eip" "alb" {
+#  resource "aws_eip" "alb" {
 
- }
+#  }
 
  
